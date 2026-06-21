815577f9c9cd894a035ecaa9f89bd0be96e9ccd5
KuroiiDev <maitsamkadzim26@gmail.com>
2026-06-21 15:47:06 +0700

    chore: add comments to express server configuration

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+// server.js
+
+// Importing Dependencies
 import dotenv from "dotenv";
 import express from "express";
 
+// load config
 dotenv.config();
 
+// initialize express app
 const app = express();
+
+// defining port
 const PORT = process.env.PORT || 3000;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// start server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
